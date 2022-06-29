492996bb8071768a2f4d94e5d82fba6d43e64183
Walt Boxwell <waltboxwell@gmail.com>
2022-06-29 13:36:11 -0500

    NEED TO INITIALIZE PRODUCT MODEL

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,32 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+    },
+    stock: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
